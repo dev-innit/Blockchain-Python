1d1c5c20e70fdd047e9b99b3b4bf25d6b0a1b97e
dev-innit <181482971+dev-innit@users.noreply.github.com>
2026-09-14 16:20:53 +0300

    a function for transaction

--- webserver.py
+++ webserver.py
@@ -7,3 +7,20 @@ node = Flask(__name__)
 this_transcations = []
 
 @node.route('/transcations/new', methods=['POST'])
+
+def new_transcation():
+    if request.method == 'POST':
+#on each new we extract the transcation data
+        new_transcation = request.get_json()
+        this_transcations.append(new_transcation)
+        return "Transaction received!"
+# since the transcations were succefully added we can return a success message
+        print ("New transcation added: ")
+        print ("FROM: {}".format(new_transcation['from']))
+        print ("TO: {}".format(new_transcation['to']))
+        print ("AMOUNT: {}".format(new_transcation['amount']))
+        
+        #then we let the clienn know it was successful
+        return "Transaction submitted successfully!"
+    
+node.run()
