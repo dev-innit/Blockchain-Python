11894426b15f8169ef1aeef9fb93101cdc7514b3
dev-innit <181482971+dev-innit@users.noreply.github.com>
2026-09-14 14:54:37 +0300

    imported required linraries

---
+++ main.py
@@ -0,0 +1,7 @@
+#bloackchain a digital ledger in which transcations made in bitcoin or another cryptocurrency are recored chronologically and publicly
+
+
+#introducing SnakeCoin
+
+import hashlib as hasher
+
