ffc09f3e4ec698b578f21c601949fb54dcf88339
dev-innit <181482971+dev-innit@users.noreply.github.com>
2026-09-14 16:59:35 +0300

    final touches on the blockchains

--- main.py
+++ main.py
@@ -1,50 +1,175 @@
-#bloackchain a digital ledger in which transcations made in bitcoin or another cryptocurrency are recored chronologically and publicly
-
-
-#introducing SnakeCoin
-
+from flask import Flask
+from flask import request
+import json
+from urllib.request import urlopen
 import hashlib as hasher
 import datetime as date
+node = Flask(__name__)
 
+# Define what a Snakecoin block is
 class Block:
-    def __init__(self, index, timestamp, data, previous_hash):
-        self.index = index
-        self.timestamp = timestamp
-        self.data = data
-        self.previous_hash = previous_hash
-        self.hash = self.hash_block()
+  def __init__(self, index, timestamp, data, previous_hash):
+    self.index = index
+    self.timestamp = timestamp
+    self.data = data
+    self.previous_hash = previous_hash
+    self.hash = self.hash_block()
+  
+  def hash_block(self):
+    sha = hasher.sha256()
+    block_contents = (
+      str(self.index) +
+      str(self.timestamp) +
+      str(self.data) +
+      str(self.previous_hash)
+    )
+    sha.update(block_contents.encode())
+    return sha.hexdigest()
 
-    def hash_block(self):
-        sha = hasher.sha256()
-        sha.update((str(self.index) +
-                    str(self.timestamp) +
-                    str(self.data) +
-                    str(self.previous_hash)).encode())
-        return sha.hexdigest()
+# Generate genesis block
 def create_genesis_block():
-    # Manually construct a block with index zero and arbitrary previous hash
-    return Block(0, date.datetime.now(), "Genesis Block", "0")
+  # Manually construct a block with
+  # index zero and arbitrary previous hash
+  return Block(0, date.datetime.now(), {
+    "proof-of-work": 9,
+    "transactions": None
+  }, "0")
 
-def next_block(last_block):
-    this_index = last_block.index + 1
-    this_timestamp = date.datetime.now()
-    this_data = "Hey! I'm block " + str(this_index)
-    this_hash = last_block.hash
-    return Block(this_index, this_timestamp, this_data, this_hash)
+# A completely random address of the owner of this node
+miner_address = "q3nf394hjg-random-miner-address-34nf3i4nflkn3oi"
+# This node's blockchain copy
+blockchain = []
+blockchain.append(create_genesis_block())
+# Store the transactions that
+# this node has in a list
+this_nodes_transactions = []
+# Store the url data of every
+# other node in the network
+# so that we can communicate
+# with them
+peer_nodes = []
+# A variable to deciding if we're mining or not
+mining = True
 
-# blockchain and genesis block
-blockchain = [create_genesis_block()]
-previous_block = blockchain[0]
+@node.route('/txion', methods=['POST'])
+def transaction():
+  # On each new POST request,
+  # we extract the transaction data
+  new_txion = request.get_json()
+  # Then we add the transaction to our list
+  this_nodes_transactions.append(new_txion)
+  # Because the transaction was successfully
+  # submitted, we log it to our console
+  print("New transaction")
+  print("FROM: {}".format(new_txion['from']))
+  print("TO: {}".format(new_txion['to']))
+  print("AMOUNT: {}\n".format(new_txion['amount']))
+  # Then we let the client know it worked out
+  return "Transaction submission successful\n"
 
-#adding of blocks to the chain
-num_of_blocks_to_add = 31
+@node.route('/blocks', methods=['GET'])
+def get_blocks():
+  chain_to_send = []
+  # Convert our blocks into dictionaries
+  # so we can send them as json objects later
+  for block in blockchain:
+    block_index = str(block.index)
+    block_timestamp = str(block.timestamp)
+    block_data = str(block.data)
+    block_hash = block.hash
+    chain_to_send.append({
+      "index": block_index,
+      "timestamp": block_timestamp,
+      "data": block_data,
+      "hash": block_hash
+    })
+  chain_to_send = json.dumps(chain_to_send)
+  return chain_to_send
 
-#blocks adding loop
-for i in range(0, num_of_blocks_to_add):
-    block_to_add = next_block(previous_block)
-    blockchain.append(block_to_add)
-    previous_block = block_to_add
-    #print out the block number and hash
-    print("Block #{} has been added to the blockchain!".format(block_to_add.index))
-    print("Hash: {}\n".format(block_to_add.hash))
-    
+def find_new_chains():
+  # Get the blockchains of every
+  # other node
+  other_chains = []
+  for node_url in peer_nodes:
+    # Get their chains using a GET request
+    with urlopen(node_url + "/blocks") as response:
+      block = response.read()
+    # Convert the JSON object to a Python dictionary
+    block = json.loads(block)
+    # Add it to our list
+    other_chains.append(block)
+  return other_chains
+
+def consensus():
+  global blockchain
+  # Get the blocks from other nodes
+  other_chains = find_new_chains()
+  # If our chain isn't longest,
+  # then we store the longest chain
+  longest_chain = blockchain
+  for chain in other_chains:
+    if len(longest_chain) < len(chain):
+      longest_chain = chain
+  # If the longest chain isn't ours,
+  # then we stop mining and set
+  # our chain to the longest one
+  blockchain = longest_chain
+
+def proof_of_work(last_proof):
+  # Create a variable that we will use to find
+  # our next proof of work
+  incrementor = last_proof + 1
+  # Keep incrementing the incrementor until
+  # it's equal to a number divisible by 9
+  # and the proof of work of the previous
+  # block in the chain
+  while not (incrementor % 9 == 0 and incrementor % last_proof == 0):
+    incrementor += 1
+  # Once that number is found,
+  # we can return it as a proof
+  # of our work
+  return incrementor
+
+@node.route('/mine', methods = ['GET'])
+def mine():
+  # Get the last proof of work
+  last_block = blockchain[len(blockchain) - 1]
+  last_proof = last_block.data['proof-of-work']
+  # Find the proof of work for
+  # the current block being mined
+  # Note: The program will hang here until a new
+  #       proof of work is found
+  proof = proof_of_work(last_proof)
+  # Once we find a valid proof of work,
+  # we know we can mine a block so 
+  # we reward the miner by adding a transaction
+  this_nodes_transactions.append(
+    { "from": "network", "to": miner_address, "amount": 1 }
+  )
+  # Now we can gather the data needed
+  # to create the new block
+  new_block_data = {
+    "proof-of-work": proof,
+    "transactions": list(this_nodes_transactions)
+  }
+  new_block_index = last_block.index + 1
+  new_block_timestamp = this_timestamp = date.datetime.now()
+  last_block_hash = last_block.hash
+  # Empty transaction list
+  this_nodes_transactions[:] = []
+  # Now create the
+  # new block!
+  mined_block = Block(
+    new_block_index,
+    new_block_timestamp,
+    new_block_data,
+    last_block_hash
+  )
+  blockchain.append(mined_block)
+  # Let the client know we mined a block
+  return json.dumps({
+      "index": new_block_index,
+      "timestamp": str(new_block_timestamp),
+      "data": new_block_data,
+      "hash": last_block_hash
+  }) + "\n"
