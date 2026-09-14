c72276c76e7bde59b8706c60939705e2b8d2bb99
dev-innit <181482971+dev-innit@users.noreply.github.com>
2026-09-14 16:13:25 +0300

    the web server side

--- webserver.py
+++ webserver.py
@@ -1,3 +1,9 @@
 from flask import Flask
 from flask import request
 
+node = Flask(__name__)
+
+# store transcations in a list
+this_transcations = []
+
+@node.route('/transcations/new', methods=['POST'])
